9662907e6ee5ec9f275a9a8554b1a83c7de75023
Javier <javiereduardor189@gmail.com>
2025-10-10 19:55:01 -0600

    Revert "Se creó la parte de compras pero falta pulirla"
    This reverts commit 876fbb3bc0f1ade0af7cb0e31a16ae05cd0dcddc.

--- ProyectoLogin/Models/Proveedor.cs
+++ ProyectoLogin/Models/Proveedor.cs
@@ -8,7 +8,7 @@
         public string? Telefono { get; set; }
         public string? Email { get; set; }
         public string? Direccion { get; set; }
-        public bool Activo { get; set; } = true;
+        public bool Activo { get; set; }
 
         
     }
