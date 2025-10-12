1c301886bceff713a43b0637e7790038aa7bc6f6
Javier <javiereduardor189@gmail.com>
2025-10-12 09:29:02 -0600

    se eliminó la parte de compras a proveedores para mejor estructuracion

--- ProyectoLogin/Recursos/Utilidades.cs
+++ ProyectoLogin/Recursos/Utilidades.cs
@@ -7,18 +7,17 @@ namespace ProyectoLogin.Recursos
     {
         public static string EncriptarClave(string clave)
         {            
-            StringBuilder sb = new StringBuilder(); // StringBuilder se utiliza para ir construyendo la cadena final con los valores encriptados.
+            StringBuilder sb = new StringBuilder(); 
    
-            using (SHA256 hash = SHA256Managed.Create()) // Se crea una instancia del algoritmo SHA256 mediante SHA256Managed. El "using" asegura que el recurso se libere correctamente.
+            using (SHA256 hash = SHA256Managed.Create()) 
             {
                 
-                Encoding enc = Encoding.UTF8; //se convierte la clave en bytes usando codificacion UTF8
+                Encoding enc = Encoding.UTF8; 
                 
-                byte[] result = hash.ComputeHash(enc.GetBytes(clave)); // Se genera el hash a partir de la clave proporcionada.
+                byte[] result = hash.ComputeHash(enc.GetBytes(clave)); 
 
-                foreach (byte b in result) // se itera sobre cada byte del hash resultante.
-                    sb.Append(b.ToString("x2")); // "x2" indica que el valor se mostrará en formato hexadecimal de 2 dígitos.
-
+                foreach (byte b in result) 
+                    sb.Append(b.ToString("x2")); 
             }
             return sb.ToString();
         }
